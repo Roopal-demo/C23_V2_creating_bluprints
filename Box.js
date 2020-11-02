ca9013f5e01901cbb7934fe59bac96fd2fe37dce
Roopal-demo <63741128+Roopal-demo@users.noreply.github.com>
2020-11-02 20:31:05 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,18 @@
+class Box{
+    constructor(x,y,width,height){
+        var options={
+            restitution:1.0
+        }
+        this.width=width;
+        this.height=height;
+        this.body=Bodies.rectangle(x,y,width,height,options);
+        
+        World.add(world, this.body);
+    }
+    dislplay(){
+        var pos=this.body.position;
+        rectMode(CENTER);
+        fill(255);
+        rect(pos.x,pos.y,this.width,this.height);
+    }
+}
